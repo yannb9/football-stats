eb00c831c7c76304e9a1508339b61bd080da49f1
Yann Bohbot <yannbohbot@gmail.com>
2020-09-24 17:27:17 +0300

    Final Commit

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,5 @@
 export const Api = {
-    key: 'syV2v9EhhxGkNokncoYIDBUFbMUvy2wLdSAmX5TRuQRdSSo9yFmTDaZPA3cB'
+    // key: 'syV2v9EhhxGkNokncoYIDBUFbMUvy2wLdSAmX5TRuQRdSSo9yFmTDaZPA3cB',
+    key: 'ByrEzUpEHuxc0Um3ocLDvzEhEnd355IXwSK8Kt1SfaspRnfOCbp5kjsn9NbA'
 }
 
